6208071cfacc45809f0b28517ece6d4199bd0437
awais <awais.dev321@gmail.com>
2025-08-08 10:56:44 +0500

    changes

--- fronten-car-rental/src/views/BestCarsView/bestAllCars.jsx
+++ fronten-car-rental/src/views/BestCarsView/bestAllCars.jsx
@@ -15,7 +15,7 @@ export default function AllBestCars() {
     const fetchCars = async () => {
       try {
         setLoading(true);
-        const response = await axios.get('/.netlify/functions/api/api/cars');
+        const response = await axios.get('http://localhost:5000/api/cars');
 
         // Check if response has the correct structure and data is an array
         if (response.data && response.data.data && Array.isArray(response.data.data)) {
@@ -95,7 +95,7 @@ export default function AllBestCars() {
           <BaseCard width='w-[380px]' padding='p-[8px]' height='h-full' key={car._id}>
             <div className="relative h-48 overflow-hidden">
               <img
-                src={`/.netlify/functions/api/${car.coverImage}`}
+                src={`http://localhost:5000/${car.coverImage}`}
                 alt={car.name}
                 className="w-full h-full rounded-[15px] object-cover"
               />
