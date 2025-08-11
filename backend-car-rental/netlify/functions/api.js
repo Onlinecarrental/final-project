002f4af062a2ba6a8333cdb7547cb21b1de09747
awais <awais.dev321@gmail.com>
2025-08-11 10:07:35 +0500

    changes

--- backend-car-rental/netlify/functions/api.js
+++ backend-car-rental/netlify/functions/api.js
@@ -3,8 +3,17 @@ require('dotenv').config();
 const mongoose   = require('mongoose');
 const serverless = require('serverless-http');
 const app        = require('../app');
-const blogRoutes = require('./routes/blogRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
+
+const blogRoutes = require('../routes/blogRoutes');
+const categoryRoutes = require('../routes/categoryRoutes');
+const carRoutes = require('../routes/carRoutes');
+const contactRoutes = require('../routes/contactRoutes');
+const reviewRoutes = require('../routes/reviewRoutes');
+const homepageRoutes = require('../routes/homepageRoutes');
+const aboutRoutes = require('../routes/aboutRoutes');
+const chatRoutes = require('../routes/chatRoutes');
+const bookingRoutes = require('../routes/bookingRoutes');
+const paymentRoutes = require('../routes/paymentRoutes');
 
 let conn = null;
 
@@ -14,7 +23,16 @@ const connectToDatabase = async () => {
   console.log('MongoDB connected (Function bootstrap)');
 };
 
+
 app.use('/api/blogs', blogRoutes);
 app.use('/api/categories', categoryRoutes);
+app.use('/api/cars', carRoutes);
+app.use('/api/contact', contactRoutes);
+app.use('/api/reviews', reviewRoutes);
+app.use('/api/homepage', homepageRoutes);
+app.use('/api/about', aboutRoutes);
+app.use('/api/chats', chatRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/payments', paymentRoutes);
 
 module.exports.handler = serverless(app);
